7de155a5cc51bcd1a12e3b83876e1922f2bceb68
exkot00 <114568769+exkot00@users.noreply.github.com>
2022-12-29 03:59:20 +0500

    29

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package ru.kata.spring.boot_security.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByLogin(String login);
+    @Query("select u from User u join fetch u.roles where u.login = :login")
+    User findByLogin(@Param("login") String login);
 }
